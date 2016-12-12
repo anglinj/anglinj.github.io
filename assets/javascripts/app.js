bf4bef4c300c748ec51ac5e7a5d653dd898da100
Anglin, Jonathan <Jonathan.Anglin@ttigroupna.com>
2016-12-12 14:40:45 -0500

    Start work on parallax bg.

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,1 +1,10 @@
 $(document).foundation();
+
+$(window).scroll(function(e){
+  parallax();
+});
+
+function parallax(){
+  var scrolled = $(window).scrollTop();
+  $('.top-img').css('background-position','center ' + -(scrolled*0.35)+'px');
+}
