bdf8a95eb1beea7d8979fcbc66bb7d54f6664dec
Jonathan Anglin <jonathan.a.designs@gmail.com>
2017-02-08 17:02:42 -0500

    Updated layout and optimized images.

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,22 +1,22 @@
 $(document).foundation();
 
-(function($) {
-    var element = $('.follow-scroll'),
-        originalY = element.offset().top;
+// (function($) {
+//     var element = $('.follow-scroll'),
+//         originalY = element.offset().top;
 
-    // Space between element and top of screen (when scrolling)
-    var topMargin = 0;
+//     // Space between element and top of screen (when scrolling)
+//     var topMargin = 0;
 
-    // Should probably be set in CSS; but here just for emphasis
-    element.css('position', 'absolute');
+//     // Should probably be set in CSS; but here just for emphasis
+//     element.css('position', 'absolute');
 
-    $(window).on('scroll', function(event) {
-        var scrollTop = $(window).scrollTop();
+//     $(window).on('scroll', function(event) {
+//         var scrollTop = $(window).scrollTop();
 
-        element.stop(false, false).animate({
-            top: scrollTop < originalY
-                    ? 0
-                    : scrollTop - originalY + topMargin
-        }, 300);
-    });
-})(jQuery);
+//         element.stop(false, false).animate({
+//             top: scrollTop < originalY
+//                     ? 0
+//                     : scrollTop - originalY + topMargin
+//         }, 300);
+//     });
+// })(jQuery);
